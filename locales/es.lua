99e55f55a8aa5240c3da0420b6951512feba9e02
daescan11 <64392935+daescan11@users.noreply.github.com>
2020-04-27 06:05:44 +0100

    Add files via upload

---
+++ locales/es.lua
@@ -0,0 +1,16 @@
+Locales['es'] = {
+	['GPS_info'] = 'La central indicaba un bote de basura en su ~ y ~ GPS',
+	['cancel_mission'] = 'Has cancelado la misión.',
+	['pickup'] = '~INPUT_CONTEXT~ Para recoger la basura',
+	['end_service'] = 'Servicio final',
+	['take_service'] = 'Toma su servicio',
+	['end_service_notif'] = 'Fin de ~r~Service',
+	['take_service_notif'] = 'Tomando ~ g ~ Servicio',
+	['start_job'] = 'Use la tecla ~ b ~ BORRAR ~ w ~ para comenzar su turno ~ b ~',
+	['Vehicle_Menu_Title'] = 'Vehículo de servicio',
+	['in_vehicle'] = '~ r ~ ¡Sal de tu vehículo!',
+	['vehicle_broken'] = '~ r ~ ¡Ve a reparar el vehículo antes!',
+	['bad_vehicle'] = 'Solo puede almacenar ~ b ~ vehículos de servicio',
+	['not_good_veh'] = '~ r ~ Debe estar en su vehículo de servicio',
+	['stop_npc'] = 'Acción ~ r ~ imposible ~ s ~: Reunión en el centro de Prosegur',
+}
